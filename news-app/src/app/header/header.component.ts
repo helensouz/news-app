b92092644e65c174ca9b4759dab659e4d056ab6e
helensouzaoliveiraibm <sousahelen816@gmail.com>
2025-04-17 22:56:00 -0300

    header comp

--- news-app/src/app/header/header.component.ts
+++ news-app/src/app/header/header.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule], // Importando apenas o componente standalone
+  imports: [MatIconModule, MatCardModule], // Importando apenas o componente standalone
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
